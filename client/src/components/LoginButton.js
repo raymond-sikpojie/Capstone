56428573838d3f8524adc2d4a3af82ca7456ed78
raymond <raymondomon@yahoo.com>
2021-03-11 15:12:20 +0100

    update

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -3,6 +3,7 @@ import {useAuth0} from "@auth0/auth0-react"
 
 export default function LoginButton() {
     const {loginWithRedirect, isAuthenticated} = useAuth0();
+    
     return (
         !isAuthenticated ?
         <button onClick={() => loginWithRedirect()}>
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useAuth0} from "@auth0/auth0-react"
 
 export default function Profile() {
     const {user, isAuthenticated} = useAuth0()
+
+    // componentDidMount () {
+
+    // }
+
     return (
-        isAuthenticated ?
+        isAuthenticated ? 
         <div>
             <h3>{user.name}</h3>
             <p>{user.email}</p>
-            {/* {JSON.stringify(user)} */}
+            <p>{user.sub}</p>
+            {JSON.stringify(user)}
         </div>: null
     )
 }
