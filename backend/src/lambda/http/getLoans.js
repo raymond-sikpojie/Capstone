f3d3caad7dd8a1c3a8a4387f6d8efbf437071c4d
raymond <raymondomon@yahoo.com>
2021-03-11 18:22:55 +0100

    initial commit

--- backend/src/lambda/http/getLoans.js
+++ backend/src/lambda/http/getLoans.js
@@ -1,6 +1,35 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
+const loanTable = process.env.LOAN_TABLE
+const userIndex = process.env.USERID_INDEX
 
 module.exports.getAllLoans = async (event) => {
+
+    const userId = event.headers.Authorization
     
+    const result = await docClient.query({
+        TableName: loanTable,
+        IndexName: userIndex,
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": userId
+        },
+
+        ScanIndexForward: false
+    }).promise()
+
+    const items = result.Items
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items
+        })
+        // body: JSON.stringify({
+        //     "item": userId
+        // })
+    }
 }
