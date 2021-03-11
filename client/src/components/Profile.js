0fc52e22158e4c98aed96f330674a67c03d29fde
raymond <raymondomon@yahoo.com>
2021-03-11 18:18:15 +0100

    added fetch route

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,12 +1,54 @@
-import React, {useState} from 'react'
+import React, {useEffect} from 'react'
 import {useAuth0} from "@auth0/auth0-react"
+
+
 
 export default function Profile() {
     const {user, isAuthenticated} = useAuth0()
 
-    // componentDidMount () {
+    useEffect(() => {
 
-    // }
+        if (isAuthenticated) {
+         console.log("component did mount")
+        
+        //  const request = await fetch("https://5y0jnmttzk.execute-api.us-east-1.amazonaws.com/dev/loan", {
+        //     method: "POST",
+        //     credentials: "same-origin",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify(user),
+        //   });
+        
+        //   const responseData = await request.json();
+        
+        //   if (request.status === 200) {
+        //       console.log(responseData)
+        //   }
+
+        const getLoans = async () => {
+            const request = await fetch("https://bztmjaum2a.execute-api.us-east-1.amazonaws.com/dev/loan", {
+                method: "GET",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": user.sub
+                },
+            
+            })
+            const responseData = await request.json()
+            console.log(responseData);
+        }
+
+        getLoans()
+
+        }
+        
+
+
+    })
+
+    
 
     return (
         isAuthenticated ? 
